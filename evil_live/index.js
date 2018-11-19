a6a21886102b2270afed04c294581022dc3949ef
Anil Yadav <anil.yadav211995@gmail.com>
2018-11-20 00:21:23 +0530

    del

--- evil_live/index.js
+++
@@ -1,8 +0,0 @@
-let main_block = document.getElementById("works");
-
-let child_blocks = document.getElementById("clips");
-child_blocks.style.display = "none";
-main_block.onclick = function() {
-  main_block.style.display = "none";
-  child_blocks.style.display = "block";
-};
